7212c50ff839455949ad4ecfe703e6bf98947dca
navoni1024 <radec8402@gmail.com>
2026-02-25 13:41:37 +0800

    refactor old structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,94 +1,20 @@
-import './App.css';
-import 'leaflet/dist/leaflet.css';
-import React, { useState, useEffect } from 'react';
-import { MapContainer } from 'react-leaflet/MapContainer'
-import { GeoJSON } from 'react-leaflet/GeoJSON'
-import mapdata from "./geo.json"
+//import { useState } from 'react'
+//import { ThemeProvider } from 'styled-components'
+//import themes from "./themes"
+import './App.css'
+import RoamingTaiwan from './RoamingTaiwan'
 
 
-function App() {
-  //./TOWN_MOI_1120317.json
-  /*
-  const geoEvent = (mapdata.features, layer) => {
+//const defaultTheme = Object.keys(themes)[0];
 
-  };
-  */
-  /*
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      */
-     
-  let [isBingoWaiting, setIsBingoWaiting] = useState(0);
-  let [selectedTownName, setSelectedTownName] = useState();
-  let [randomQuetion, setRandomQuetion] = useState();
-  let [score, setScore] = useState(0);
+const App = () => {
+  //const [selectedTheme, setSelectedTheme] = useState(defaultTheme);
 
-  useEffect(()=>{
-    const keys = Object.keys(mapdata.features);
-    const randIndex = Math.floor(Math.random() * keys.length);
-    setRandomQuetion(randomQuetion=mapdata.features[randIndex].properties.TOWNNAME);
-  },[]);
-  
-  const bingoAction = () => {
-    const keys = Object.keys(mapdata.features);
-    const randIndex = Math.floor(Math.random() * keys.length);
-    setRandomQuetion(randomQuetion=mapdata.features[randIndex].properties.TOWNNAME);
-    setScore(score=score+1);
-    setSelectedTownName(selectedTownName="");
-    setIsBingoWaiting(isBingoWaiting=0);
-  }
-  
-  const mapFeature=(country, layer)=>{
-    layer.on({
-      mouseover: (e) => {
-        e.target.setStyle({
-          fillOpacity: 0.5,
-        });
-      },
-      mouseout: (e) => {
-        e.target.setStyle({
-          fillOpacity: 1,
-        });
-      },
-      click: (e) => {
-        if((randomQuetion===e.sourceTarget.feature.properties.TOWNNAME)&&(isBingoWaiting===0)){
-          setSelectedTownName(selectedTownName=e.sourceTarget.feature.properties.TOWNNAME);
-          setIsBingoWaiting(isBingoWaiting=1);
-          setTimeout(bingoAction,1000);
-        }
-        else if(isBingoWaiting===0){
-          setSelectedTownName(selectedTownName=e.sourceTarget.feature.properties.TOWNNAME);
-        }
-      }
-      
-    });
-  }
-  const mapStyle={
-    weight: 1,
-    fillOpacity: 1,
-    fillColor: "rgb(128, 206, 197)",
-    color: "rgb(230,230,230)",
-  };
-
-  const mapBound =[
-    [26.504979796639104, 116.100698791452],
-    [20.67667721806277, 125.49054604625438],
-  ]
-
-  return (
-    <div className='container'>
-      <MapContainer center={[23.6, 120.9738819]} zoom={7} minZoom={7} maxBounds={mapBound}>
-      <GeoJSON style={mapStyle} data={mapdata} onEachFeature={mapFeature}></GeoJSON>
-      </MapContainer>
-      <div className='right' align="center" style={{backgroundColor: isBingoWaiting? "rgb(67, 247, 67)": "rgba(204, 204, 204, 0.457)"}}>
-        <div className='upper'>題目：{randomQuetion}</div>
-        <div className='middle'>所選擇的區域：{selectedTownName}</div>
-        <div className='score'>分數：<b>{score}</b></div>
-      </div>
-    </div>
-  ); 
+  return(
+    //<ThemeProvider theme={themes[selectedTheme]}>
+      <RoamingTaiwan />
+    //</ThemeProvider>
+  )
 }
 
-export default App;
+export default App
