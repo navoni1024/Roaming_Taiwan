e8dc7aeb84e69fab9b97c501b6cdd74434a246c1
navoni1024 <radec8402@gmail.com>
2026-02-25 14:38:09 +0800

    refactor component

--- client/src/RoamingTaiwan.jsx
+++ client/src/RoamingTaiwan.jsx
@@ -1,53 +1,57 @@
 import 'leaflet/dist/leaflet.css';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { GeoJSON } from 'react-leaflet/GeoJSON'
 import mapdata from "./geojson/geo.json"
+import StatusBar from './components/StatusBar'
 
 const RoamingTaiwan = () => {
+
     let [isBingoWaiting, setIsBingoWaiting] = useState(0);
     let [selectedTownName, setSelectedTownName] = useState();
     let [randomQuetion, setRandomQuetion] = useState();
     let [score, setScore] = useState(0);
 
+    //這裡好像是初始化
     useEffect(()=>{
         const keys = Object.keys(mapdata.features);
         const randIndex = Math.floor(Math.random() * keys.length);
         setRandomQuetion(randomQuetion=mapdata.features[randIndex].properties.TOWNNAME);
     },[]);
 
+
     const bingoAction = () => {
-    const keys = Object.keys(mapdata.features);
-    const randIndex = Math.floor(Math.random() * keys.length);
-    setRandomQuetion(randomQuetion=mapdata.features[randIndex].properties.TOWNNAME);
+        const keys = Object.keys(mapdata.features);
+        const randIndex = Math.floor(Math.random() * keys.length);
+        setRandomQuetion(randomQuetion=mapdata.features[randIndex].properties.TOWNNAME);
         setScore(score=score+1);
         setSelectedTownName(selectedTownName="");
         setIsBingoWaiting(isBingoWaiting=0);
     }
 
-    const mapFeature=(country, layer)=>{
-    layer.on({
-        mouseover: (e) => {
-            e.target.setStyle({
-                fillOpacity: 0.5,
-            });
-        },
-        mouseout: (e) => {
-            e.target.setStyle({
-                fillOpacity: 1,
-            });
-        },
-        click: (e) => {
-            if((randomQuetion===e.sourceTarget.feature.properties.TOWNNAME)&&(isBingoWaiting===0)){
-                setSelectedTownName(selectedTownName=e.sourceTarget.feature.properties.TOWNNAME);
-                setIsBingoWaiting(isBingoWaiting=1);
-                setTimeout(bingoAction,1000);
+    const mapFeature=(layer)=>{
+        layer.on({
+            mouseover: (e) => {
+                e.target.setStyle({
+                    fillOpacity: 0.5,
+                });
+            },
+            mouseout: (e) => {
+                e.target.setStyle({
+                    fillOpacity: 1,
+                });
+            },
+            click: (e) => {
+                if((randomQuetion===e.sourceTarget.feature.properties.TOWNNAME)&&(isBingoWaiting===0)){
+                    setSelectedTownName(selectedTownName=e.sourceTarget.feature.properties.TOWNNAME);
+                    setIsBingoWaiting(isBingoWaiting=1);
+                    setTimeout(bingoAction,1000);
+                }
+                else if(isBingoWaiting===0){
+                    setSelectedTownName(selectedTownName=e.sourceTarget.feature.properties.TOWNNAME);
+                }
             }
-            else if(isBingoWaiting===0){
-                setSelectedTownName(selectedTownName=e.sourceTarget.feature.properties.TOWNNAME);
-            }
-        }
-    });
+        });
     }
 
     const mapStyle={
@@ -64,14 +68,18 @@ const RoamingTaiwan = () => {
 
     return (
         <div className='container'>
+
             <MapContainer center={[23.6, 120.9738819]} zoom={7} minZoom={7} maxBounds={mapBound}>
-            <GeoJSON style={mapStyle} data={mapdata} onEachFeature={mapFeature}></GeoJSON>
+                <GeoJSON style={mapStyle} data={mapdata} onEachFeature={mapFeature}></GeoJSON>
             </MapContainer>
-            <div className='right' align="center" style={{backgroundColor: isBingoWaiting? "rgb(67, 247, 67)": "rgba(204, 204, 204, 0.457)"}}>
-            <div className='upper'>題目：{randomQuetion}</div>
-            <div className='middle'>所選擇的區域：{selectedTownName}</div>
-            <div className='score'>分數：<b>{score}</b></div>
-            </div>
+
+            <StatusBar 
+                randomQuetion={randomQuetion} 
+                selectedTownName={selectedTownName}
+                score={score}
+                isBingoWaiting={isBingoWaiting}
+            />
+
         </div>
     ); 
 }
