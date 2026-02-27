72f3406a67376635b7787921c706b203d2d34e80
navoni1024 <radec8402@gmail.com>
2026-02-28 01:18:31 +0800

    adjust ratio between map & sidebar

--- client/src/components/StatusBar/index.jsx
+++ client/src/components/StatusBar/index.jsx
@@ -1,29 +1,34 @@
 
-const gameModeStatus = ({gameMode, timer, questionRemain}) => {
-    if(gameMode === 'timeLimit'){
-        return(
-            <>
-                <div>剩餘時間: {timer}</div>
-            </>
-        )
-    }else if(gameMode === 'quesionComplete'){
-        return(
-            <>
-                <div>花費時間: {timer}</div>
-                <div>剩餘題目: {questionRemain}</div>
-            </>
-        )
+const StatusBar = ({ gameMode, questionTown, selectedTownName, timer, questionRemain, correctAnswerCount = 0, wrongAnswerCount = 0, isBingoWaiting}) => {
+
+    const GameModeInfo = () => {
+        if(gameMode === 'timeLimit'){
+            return(
+                <>
+                    <div>剩餘時間: {timer}</div>
+                    <div className='score'>答題情況: <b>{correctAnswerCount}</b></div>
+                </>
+            )
+        }else if(gameMode === 'questionsComplete'){
+            return(
+                <>
+                    <div>剩餘題目: {questionRemain}</div>
+                    <div>花費時間: {timer}</div>
+                </>
+            )
+        }else{
+            return(
+                <>
+                </>
+            )
+        }
     }
-}
 
-
-const StatusBar = ({ questionTown, selectedTownName, timer, questionRemain, correctAnswerCount = 0, wrongAnswerCount = 0, isBingoWaiting}) => {
     return(
         <div className='section status-bar' align="center" style={{backgroundColor: isBingoWaiting? "rgb(67, 247, 67)": "rgb(255, 255, 255)"}}>
             <div className='upper'>題目：{questionTown}</div>
             <div className='middle'>所選區域：{selectedTownName}</div>
-            
-            <div className='score'>答題情況: <b>{correctAnswerCount}</b></div>
+            <GameModeInfo />
         </div>
     )
 }
